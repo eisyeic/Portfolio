53bf8eba67ae8cd3b15620059975c045919be327
eisyeic <davideisenbarth@gmail.com>
2026-01-22 23:53:16 +0100

    next media query

--- src/app/shared/home/my-projects/say-about-me/say-about-me/say-about-me.component.ts
+++ src/app/shared/home/my-projects/say-about-me/say-about-me/say-about-me.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { TranslateModule } from "@ngx-translate/core";
+import { NgStyle } from "@angular/common";
 
 @Component({
   selector: 'app-say-about-me',
   standalone: true,
-  imports: [ TranslateModule ],
+  imports: [TranslateModule, NgStyle],
   templateUrl: './say-about-me.component.html',
   styleUrl: './say-about-me.component.scss'
 })
