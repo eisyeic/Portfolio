3381c13a7126ab68a306b002dbf59c3ad418c49c
eisyeic <davideisenbarth@gmail.com>
2026-01-25 23:50:58 +0100

    finsh navbar mobile and next step media query

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,21 +1,79 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
+import { Router } from '@angular/router';
+import { ViewportScroller } from '@angular/common';
+import { inject } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, TranslateModule],
+  imports: [CommonModule, TranslateModule,],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  private router = inject(Router);
+  private viewportScroller = inject(ViewportScroller);
+
+  currentLang = this.translateService.currentLang || 'en';
+  menuOpen = false;
+
+  constructor(private translateService: TranslateService) {
+    this.translateService.onLangChange.subscribe(event => {
+      this.currentLang = event.lang;
+    });
+  }
+
+  onMenuIconClick() {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  closeMenu() {
+    this.menuOpen = false;
+  }
+
+  switchLanguage(lang: string) {
+    this.translateService.use(lang);
+    this.currentLang = lang;
+  }
   openEmail() {
     window.location.href = 'mailto:davideisenbarth@gmail.com';
   }
 
   scrollToTop() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
+    this.closeMenu();
+  }
+
+  onLogoClick(): void {
+    if (this.router.url === '/' || this.router.url === '') {
+      this.viewportScroller.scrollToPosition([0, 0]);
+    } else {
+      this.router.navigate(['/']).then(() => {
+        this.viewportScroller.scrollToPosition([0, 0]);
+      });
+    }
+  }
+
+  scrollToSection(sectionId: string) {
+    if (this.router.url === '/' || this.router.url.startsWith('/#')) {
+      const el = document.getElementById(sectionId);
+      if (el) {
+        el.scrollIntoView({ behavior: 'smooth' });
+      }
+    } else {
+      this.router.navigate(['/']).then(() => {
+        setTimeout(() => {
+          const el = document.getElementById(sectionId);
+          if (el) {
+            el.scrollIntoView({ behavior: 'smooth' });
+          }
+        }, 500);
+      });
+    }
+    this.closeMenu();
   }
 }
 
