0bdcf97b29af3fcd3d79d1bf42ae244c5573c0a9
eisyeic <davideisenbarth@gmail.com>
2026-01-26 23:59:25 +0100

    responsive design completed and files organized

--- src/app/shared/home/my-projects/say-about-me/say-about-me/say-about-me.component.ts
+++ src/app/shared/home/my-projects/say-about-me/say-about-me/say-about-me.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { TranslateModule } from "@ngx-translate/core";
 import { NgStyle } from "@angular/common";
+import { AfterViewInit, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-say-about-me',
@@ -9,6 +10,57 @@ import { NgStyle } from "@angular/common";
   templateUrl: './say-about-me.component.html',
   styleUrl: './say-about-me.component.scss'
 })
-export class SayAboutMeComponent {
 
+export class SayAboutMeComponent implements AfterViewInit {
+    // ... existing code ...
+    @ViewChild('bubbleContainer', { static: false }) bubbleContainer!: ElementRef;
+
+    ngAfterViewInit() {
+        const container = this.bubbleContainer.nativeElement as HTMLElement;
+        let isDown = false;
+        let startX: number;
+        let scrollLeft: number;
+
+        // Maus-Events
+        container.addEventListener('mousedown', (e: MouseEvent) => {
+            isDown = true;
+            startX = e.pageX - container.offsetLeft;
+            scrollLeft = container.scrollLeft;
+        });
+
+        container.addEventListener('mouseleave', () => {
+            isDown = false;
+        });
+
+        container.addEventListener('mouseup', () => {
+            isDown = false;
+        });
+
+        container.addEventListener('mousemove', (e: MouseEvent) => {
+            if (!isDown) return;
+            e.preventDefault();
+            const x = e.pageX - container.offsetLeft;
+            const walk = (x - startX) * 2; // Geschwindigkeit
+            container.scrollLeft = scrollLeft - walk;
+        });
+
+        // Touch-Events
+        container.addEventListener('touchstart', (e: TouchEvent) => {
+            isDown = true;
+            startX = e.touches[0].pageX - container.offsetLeft;
+            scrollLeft = container.scrollLeft;
+        });
+
+        container.addEventListener('touchend', () => {
+            isDown = false;
+        });
+
+        container.addEventListener('touchmove', (e: TouchEvent) => {
+            if (!isDown) return;
+            const x = e.touches[0].pageX - container.offsetLeft;
+            const walk = (x - startX) * 2;
+            container.scrollLeft = scrollLeft - walk;
+        });
+    }
+    // ... existing code ...
 }
